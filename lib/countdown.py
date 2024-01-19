3221c60c338f8d63c8eeede661eae8e67b05bb69
tiffmaina@gmail.com <tiff_shiro@DESKTOP-Q44J8P0.>
2024-01-19 22:28:34 +0300

    completed

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,18 @@
 # your code goes here!
+import time
+
+def countdown(num):
+    pass
+    while num > 0:
+        print(f'{num} SECOND(S)!')
+        num -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(num):
+    pass
+    while num > 0:
+        print(f'{num} SECOND(S)!')
+        num -= 1
+        time.sleep(num)
+    print("HAPPY NEW YEAR!")
+
